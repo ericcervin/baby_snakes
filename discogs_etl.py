4f59de31a7ad8965ebf5ac31554b84710996cd7b
Eric Ervin <ericcervin@gmail.com>
2025-10-26 00:11:36 -0400

    added sqlite output

--- discogs_etl.py
+++ discogs_etl.py
@@ -1,15 +1,22 @@
 import pandas as pd
+import sqlite3
 
 def extract_from_csv(file_to_process): 
   dataframe = pd.read_csv(file_to_process) 
   return dataframe
 
-def export_csv(target_file, data): 
-    data.to_csv(target_file) 
+def load_csv(target_file, data): 
+    data.to_csv(target_file)
 
+def load_sql(target_db,target_table, data):
+    conn = sqlite3.connect(target_db)
+    data.to_sql(target_table, conn, if_exists='replace', index=False)
+    conn.close()
+    
 def main():
    df = extract_from_csv("staging/discogs-collection.csv")
-   export_csv('databases/all_discogs.csv', df)
+   load_csv('databases/all_discogs.csv', df)
+   load_sql('databases/discogs.db','release', df)
    #df.info()
    
 main()
