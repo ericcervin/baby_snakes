5801559062a831055008133f280a00a2e14c8e08
Eric Ervin <ericcervin@gmail.com>
2025-10-25 23:02:30 -0400

    added csv output

--- discogs_etl.py
+++ discogs_etl.py
@@ -4,8 +4,12 @@ def extract_from_csv(file_to_process):
   dataframe = pd.read_csv(file_to_process) 
   return dataframe
 
+def export_csv(target_file, data): 
+    data.to_csv(target_file) 
+
 def main():
    df = extract_from_csv("staging/discogs-collection.csv")
-   df.info()
+   export_csv('databases/all_discogs.csv', df)
+   #df.info()
    
 main()
