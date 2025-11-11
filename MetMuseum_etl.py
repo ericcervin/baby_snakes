f6fbb7ae50cf53868b5e1c0ae50ee531881dfd57
Eric Ervin <ericcervin@gmail.com>
2025-11-10 22:11:11 -0500

    added databases/met_art_from_new_york.csv export

--- MetMuseum_etl.py
+++ MetMuseum_etl.py
@@ -2,13 +2,22 @@ import pandas as pd
 import sqlite3
 
 def extract_from_csv(file_to_process): 
-  dataframe = pd.read_csv(file_to_process) 
+  dataframe = pd.read_csv(file_to_process,low_memory=False) 
   return dataframe
+
+def load_csv(target_file, data): 
+    data.to_csv(target_file)
 
     
 def main():
    df = extract_from_csv("staging/MetObjects.csv")
    #use in BigQuery public data or download from https://github.com/metmuseum/openaccess
-   df.info()
+   #df.info()
+
+   ny_df = df.query('City == "New York"')
+   #ny_df.info()
+
+   load_csv('databases/met_art_from_new_york.csv', ny_df)
+   
    
 main()
